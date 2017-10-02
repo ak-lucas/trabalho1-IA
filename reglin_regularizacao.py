4fa6639c50108a2a6689b992f16e7109d3a3eeb1
Kymberlim Ribeiro <kymberlim.ribeiro@gmail.com>
2017-10-01 23:42:14 -0400

    atualização

--- reglin_regularizacao.py
+++ reglin_regularizacao.py
@@ -12,31 +12,34 @@ from experimentos import ModelSelection
 # PARAMETROS
 epocas = int(sys.argv[1])
 alpha = float(sys.argv[2])
-graus = int(sys.argv[3])
-imagename = sys.argv[4]
+degree = int(sys.argv[3])
+lambdas = [float(a) for a in (sys.argv[4]).split(',')]
+imagename = sys.argv[5]
 
 # INICIALIZAÇÃO
 RL = RegularizedLinearRegression()
 MS = ModelSelection()
-LAMBDA = 0
+colors = ['red', 'blue', 'black', 'magenta', 'gray', 'yellow', 'green',  'cyan', 'orange', 'pink']
 fold = 0	
 erroTreino = []
 erroValidacao = []
 
 # CARREGA DATASET
 dataset = Dataset("datasets/combined_cycle_power_plant_dataset.csv", 1)
+dataset.dataset_scaling()
+dataset.init_polynomial()
+for d in xrange(1,degree + 1):
+	dataset.generate_polynomial_attributes(d)
 
 # 
-for train,val in MS.k_fold(dataset.X, k=5, shuffle=True):
+for train,val in MS.k_fold(dataset.X_polinomial, k=5, shuffle=True):
 	# inicialização para cada fold
-	dataset.init_polynomial()
 	erroTreino.append([])
 	erroValidacao.append([])
 
-	for g in xrange(1,graus + 1):
+	for l in lambdas:
 		# ajusta o modelo
-		dataset.generate_polynomial_attributes(g)
-		RL.fit(dataset.X_polinomial[train], dataset.Y[train], epochs=epocas, learning_rate=alpha, Lambda=LAMBDA)
+		RL.fit(dataset.X_polinomial[train], dataset.Y[train], epochs=epocas, learning_rate=alpha, Lambda=l)
 
 		# calcula e guarda o erro no treino
 		Y_pred = RL.predict(dataset.X_polinomial[train])
@@ -56,7 +59,7 @@ meanValidacao = np.asarray(erroValidacao).mean(axis=0)
 stdeviationValidacao = np.asarray(erroValidacao).std(axis=0)
 
 fig, ax = plt.subplots()
-x = range(1, graus + 1)
+x = range(len(lambdas))
 
 ax.plot(x, meanTreino, color='red', label=u"$Erro_{treino}$", linewidth=1, marker='*')
 ax.fill_between(x, meanTreino-stdeviationTreino, meanTreino+stdeviationTreino, color='red', alpha=0.2)
@@ -64,7 +67,7 @@ ax.plot(x, meanValidacao, color='blue', label=u"$Erro_{validação}$", linewidth
 ax.fill_between(x, meanValidacao-stdeviationValidacao, meanValidacao+stdeviationValidacao, color='blue', alpha=0.2)
 
 #plt.ylim([0.0, 350])
-plt.xlabel(u'$\eta$')
+plt.xlabel(u'$\lambda$')
 plt.ylabel(u'Erro')
 
 box = ax.get_position()
@@ -73,6 +76,6 @@ ax.set_position([box.x0, box.y0 + box.height * 0.1,
 lgd = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), fancybox=True, shadow=True, ncol=5)
 plt.grid(True)
 
-fig.savefig('imagens/atributos_polinomiais/ccpp_%s.png' % imagename, bbox_extra_artists=(lgd,), bbox_inches='tight')
+fig.savefig('imagens/regularizacao/ccpp_%s.png' % imagename, bbox_extra_artists=(lgd,), bbox_inches='tight')
 plt.close(fig)
 
